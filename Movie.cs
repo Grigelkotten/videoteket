48105188b2f95c04367f5305813a0e93d3a45148
Alexander Asplén <alexander.asplen@gmail.com>
2022-10-26 11:14:15 +0200

    movie changes

--- Movie.cs
+++ Movie.cs
@@ -7,8 +7,22 @@ class Movie
     public string Title { get; set; }
     public string Genre { get; set; }
     public int Price { get; set; }
+    public int LoanDays { get; set; }
     public override string ToString()
     {
         return $"{Title} {Genre}";
+    }
+    public void IsMovieOld()
+    {
+        if(IsOld == 0)
+        {
+            Price = 29;
+            LoanDays = 1;
+        }
+        else
+        {
+            Price = 49;
+            LoanDays = 2;
+        }
     }
 }
