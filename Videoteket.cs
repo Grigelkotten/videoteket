307121b9cb24e5155a0eefb8a44390c6d726dbf0
Tricksterone <johan.karlsson089@gmail.com>
2022-10-27 14:48:07 +0200

    i

--- Videoteket.cs
+++ Videoteket.cs
@@ -30,10 +30,11 @@ class Videoteket
         var movies = connection.Query<Movie>($"SELECT title FROM movies WHERE title = '{search}';").ToList();
         return movies;
     }
-    public bool IsMovieAvalible(MySqlConnection connection, string search)
+    public int IsMovieAvalible(MySqlConnection connection, string search)
     {
-        int movieId = connection.Query<Movie>($"SELECT id FROM movies WHERE title = '{search}';");
+        int movieId = connection.QuerySingle<int>($"SELECT id FROM movies WHERE title = '{search}';");
         return movieId;
+
     }
 
 }
